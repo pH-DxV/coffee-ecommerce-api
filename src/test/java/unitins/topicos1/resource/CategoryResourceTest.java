195358758d1569c5eae9ee7edce1e13b07607ac4
Raphael O. C. Leite <raphaelocl@tce-to.tce.to.gov.br>
2025-05-30 15:54:59 -0300

    Terceiro teste de sincronizaçao com a branch

--- src/test/java/unitins/topicos1/resource/CategoryResourceTest.java
+++ src/test/java/unitins/topicos1/resource/CategoryResourceTest.java
@@ -2,7 +2,6 @@ package unitins.topicos1.resource;
 
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.http.ContentType;
-import io.restassured.response.Response;
 import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.BeforeEach;
@@ -14,7 +13,6 @@ import unitins.topicos1.Category.repository.CategoryRespository;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasSize;
-import static org.hamcrest.Matchers.notNullValue;
 
 @QuarkusTest
 public class CategoryResourceTest {
